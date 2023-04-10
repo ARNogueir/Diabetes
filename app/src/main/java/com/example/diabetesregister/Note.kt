3de77a85f82556372d636bd1b46ae8b0819674b1
andre <anogueiracb@gmail.com>
2023-04-10 02:39:27 +0100

    Create Dao interface and Note Entities

---
+++ app/src/main/java/com/example/diabetesregister/Note.kt
@@ -0,0 +1,16 @@
+package com.example.diabetesregister
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notesTable")
+
+class Note(
+    @ColumnInfo(name = "title") val noteTitle: String,
+    @ColumnInfo(name = "description") val noteDescription: String,
+    @ColumnInfo(name = "timeStamp") val noteTimeStamp: String,
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id = 0
+}
